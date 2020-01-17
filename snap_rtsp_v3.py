f783a0e24f98fbad1c8dca588dded42eb83d0eb3
ChenZhouUC <chenzhou@uchicago.edu>
2020-01-17 15:44:23 +0800

    err handler added & bug fixed

--- snap_rtsp_v3.py
+++ snap_rtsp_v3.py
@@ -5,6 +5,22 @@ import time
 import os
 from copy import deepcopy
 from tqdm import tqdm
+import sys
+import traceback
+
+""" # Programme Parameter
+This is the preset programme parameter.
+"""
+
+MAX_GAP = 10
+MIN_GAP = 0
+
+FRAME_INTERVAL = 25
+SHOW_OPT = True
+
+""" # Camera Parameter
+This is the preset HikVision camera parameter.
+"""
 
 GAP_W = 8
 GAP_H = 24
@@ -15,9 +31,6 @@ START_H = 0
 
 MAX_POS_W = 5
 MAX_POS_H = 3
-
-MAX_GAP = 10
-MIN_GAP = 0
 
 
 def get_rect(pos_x, pos_y):
@@ -33,7 +46,8 @@ def convert_std(img_mat, top=1):
         img_mat = cv2.cvtColor(img_mat, cv2.COLOR_BGR2GRAY)
     digit_map = cv2.threshold(
         img_mat, 0, top, cv2.THRESH_OTSU | cv2.THRESH_BINARY)[1]
-    if (digit_map[0, :].sum() + digit_map[-1, :].sum()+digit_map[:, 0].sum()+digit_map[:, -1].sum())/(W+H-2)/2 >= top/2:
+    if (digit_map[0, :].sum() + digit_map[-1, :].sum() + digit_map[:, 0].sum()
+            + digit_map[:, -1].sum())/((W + H - 2)*2) >= top/2:
         digit_map = cv2.threshold(
             img_mat, 0, top, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)[1]
     return digit_map
@@ -50,7 +64,7 @@ def matrix_similarity(mat_A, mat_B):
     return 1 - (cv2.absdiff(mat_A, mat_B)).mean()
 
 
-def decide_num(cand, digits, crit=0.9):
+def decide_num(cand, digits, crit=0.8):
     score = 0
     idx = 0
     for i, d in enumerate(digits):
@@ -74,14 +88,16 @@ def increase_check(total, last_total):
 
 if __name__ == '__main__':
 
-    pbar = tqdm(range(10))
+    # Loading Digits Sample
     print("================>\nDigits Loading...")
+    pbar = tqdm(range(10))
     digits = []
     for i, d in enumerate(pbar):
         digits.append(load_digits(i))
-        pbar.set_description("Loading %s"%d)
+        pbar.set_description("Loading %s" % d)
         # cv2.imshow(str(i), digits[-1])
         # print(digits[-1])
+    # pbar.close()
     print("Digits Loaded...\n================>")
 
     cap = cv2.VideoCapture(
@@ -92,6 +108,7 @@ if __name__ == '__main__':
         ret, frame = cap.read()
         if ret:
             try:
+                ## Labeling Part ##
                 # position = [1, 1, 11]
                 # l, t, r, d = get_rect(position[0], position[1])
                 # crop = frame[(t+1):d, (l+1):r]
@@ -100,6 +117,7 @@ if __name__ == '__main__':
                 #                       (0, 255, 0, 255), 1, 1)
                 if counter == 0:
 
+                    ## OCR Part ##
                     count_list = []
                     for pos_h in range(MAX_POS_H):
                         temp_num = ''
@@ -110,27 +128,37 @@ if __name__ == '__main__':
                             temp_digit = decide_num(temp_mat, digits)
                             if temp_digit >= 0:
                                 temp_num += str(temp_digit)
-                                cv2.imshow(str(pos_h)+str(pos_w), crop)
+                                if SHOW_OPT:
+                                    cv2.imshow(
+                                        str(pos_h)+str(pos_w), temp_mat*255)
                             else:
                                 break
-                        count_list.append(int(temp_num))
+                        if temp_num != '':
+                            count_list.append(int(temp_num))
+                        else:
+                            pass
 
-                    if len(count_list_last) > 0 and increase_check(count_list, count_list_last):
-                        print(count_list)
-                        count_list_last = deepcopy(count_list)
-                    elif len(count_list_last) == 0:
-                        print(count_list)
-                        count_list_last = deepcopy(count_list)
+                    if len(count_list) == MAX_POS_H:
+                        if (len(count_list_last) == MAX_POS_H and increase_check(count_list, count_list_last)) or (len(count_list_last) == 0):
+                            print(count_list)
+                            count_list_last = deepcopy(count_list)
 
                 counter += 1
-                if counter == 25:
+                if counter >= FRAME_INTERVAL:
                     counter = 0
 
-                cv2.imshow('rtsp', frame)
+                if SHOW_OPT:
+                    cv2.imshow('rtsp', frame)
+
                 cv2.waitKey(30)
 
             except Exception as e:
-                print(e)
+                print('********************************************************')
+                print(sys.exc_info()[0])
+                print(sys.exc_info()[1])
+                print(traceback.extract_tb(sys.exc_info()[2])[0])
+                print('********************************************************')
+                time.sleep(1)
                 continue
         else:
             print("Camera Disconnected! Try Reconnecting...")
